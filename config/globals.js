7f85cc60566960fe2c7e0b05ec7eeea3ae9cdb89
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-03-15 11:30:51 -0400

    first commit

---
+++ config/globals.js
@@ -0,0 +1,9 @@
+/**
+ * Created by RFreeman on 2/8/2017.
+ */
+
+// array of global variables
+module.exports = {
+    // db: 'mongodb://localhost/comp2068-w2017' // local mongodb
+    db: 'mongodb://w2017dba:****@ds145669.mlab.com:45669/comp2068-w2017'  // mlab
+};
